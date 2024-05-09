fbc97ad02cbdd5b69373f58a2c7ca5b7eb55d554
Saint. Clever <parchment@gmx.com>
2024-05-09 13:35:25 -0400

    Create chapter 3 - Comparison Operators and add questions for app

---
+++ app/chapters/05 - Conditional Statements/05 - Conditional Statements.py
@@ -0,0 +1,19 @@
+# What does this Python expression evaluate to: 5 == 5?
+
+# If x = 8, what will x > 10 evaluate to?
+
+# If y = 3, what will y != 2 evaluate to?
+
+# What does 6 < 4 evaluate to?
+
+# If a = 7, what will a >= 7 evaluate to?
+
+# What does 10 == 10.0 evaluate to?
+
+# If b = 12, what will b <= 10 evaluate to?
+
+# What does 4 != 4 evaluate to?
+
+# If c = 5, what will c < 5 evaluate to?
+
+# What does 3 > 5 evaluate to?
