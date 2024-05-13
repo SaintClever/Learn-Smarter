e7abf2ee05714935c2ab1a7a72633afb604ba0e1
Saint. Clever <parchment@gmx.com>
2024-05-12 20:02:57 -0400

    Add routering

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, APIRouter, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -8,7 +8,17 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+router = APIRouter()
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/book", response_class=HTMLResponse)
+def book(request: Request):
+    return templates.TemplateResponse("book.html", {"request": request})
+
+
+
+app.include_router(router)
