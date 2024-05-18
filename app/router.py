6d5d5340b5b991af833a287d933ebf8b0d727bed
Saint. Clever <parchment@gmx.com>
2024-05-18 07:55:30 -0400

    Separate styles andd scripts by using include

--- app/router.py
+++ app/router.py
@@ -1,11 +1,8 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import json
-
 
 templates = Jinja2Templates(directory="templates")
-coding_examples = json.load(open("coding_examples.json", "r"))
 router = APIRouter()
 
 @router.get("/", response_class=HTMLResponse)
@@ -14,4 +11,4 @@ async def index(request: Request):
 
 @router.get("/book", response_class=HTMLResponse)
 async def book(request: Request):
-    return templates.TemplateResponse("base.html", {"request": request, "coding_examples": coding_examples})
+    return templates.TemplateResponse("base.html", {"request": request})
