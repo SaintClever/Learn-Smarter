ec6eb6b99d19cade2bbecd689a5624f9e2d4bb6c
Saint. Clever <parchment@gmx.com>
2024-05-31 12:22:05 -0400

    Update router, templates, and static files

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from router import router
+from .router import router, templates
+import os
 
 app = FastAPI()
 
 # Include the router
 app.include_router(router)
 
+# Set up static files
+static = os.path.join(os.path.dirname(__file__), "static")
+
 # Mount the static files directory
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory=static), name="static")
--- app/router.py
+++ app/router.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import os
 
-templates = Jinja2Templates(directory="templates")
+# Set up Jinja2 templates
+templates_dir = os.path.join(os.path.dirname(__file__), "templates")
+templates = Jinja2Templates(directory=templates_dir)
+
 router = APIRouter()
+
 
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @router.get("/book", response_class=HTMLResponse)
 async def book(request: Request):
