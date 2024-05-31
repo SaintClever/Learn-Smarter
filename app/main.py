32f299657de20b808ae899059bf6a70d9a25950a
Saint. Clever <parchment@gmx.com>
2024-05-31 12:01:23 -0400

    Undo changes made to static folder and router

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from .router import router
-import os
+from router import router
 
 app = FastAPI()
 
 # Include the router
 app.include_router(router)
 
-# Ensure the static directory exists
-static = os.path.join(os.path.dirname(__file__), "static")
-
 # Mount the static files directory
-app.mount("/static", StaticFiles(directory=static), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
