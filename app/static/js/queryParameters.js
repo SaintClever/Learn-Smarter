92c921add64946c105d6e1412940e658b3b0b4b5
Saint. Clever <parchment@gmx.com>
2024-06-01 17:54:38 -0400

    Major linking update. Added links to chapters to challenges

---
+++ app/static/js/queryParameters.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let currentURL = window.location.href;
+
+  if (currentURL.indexOf("/challenges#1") !== -1) {
+    document.getElementsByTagName('a')[1].click();
+  }
+
+  if (currentURL.indexOf("/challenges#2") !== -1) {
+    document.getElementsByTagName('a')[2].click();
+  }
+
+  if (currentURL.indexOf("/challenges#3") !== -1) {
+    document.getElementsByTagName('a')[3].click();
+  }
+
+  if (currentURL.indexOf("/challenges#4") !== -1) {
+    document.getElementsByTagName('a')[4].click();
+  }
+
+  if (currentURL.indexOf("/challenges#5") !== -1) {
+    document.getElementsByTagName('a')[5].click();
+  }
+
+  if (currentURL.indexOf("/challenges#6") !== -1) {
+    document.getElementsByTagName('a')[6].click();
+  }
+
+  if (currentURL.indexOf("/challenges#7") !== -1) {
+    document.getElementsByTagName('a')[7].click();
+  }
+
+  if (currentURL.indexOf("/challenges#8") !== -1) {
+    document.getElementsByTagName('a')[8].click();
+  }
+
+  if (currentURL.indexOf("/challenges#9") !== -1) {
+    document.getElementsByTagName('a')[9].click();
+  }
+
+  if (currentURL.indexOf("/challenges#10") !== -1) {
+    document.getElementsByTagName('a')[10].click();
+  }
+
+});
