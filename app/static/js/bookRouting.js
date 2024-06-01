de0c818bcfebd43b7e4ce9205497118408216011
Saint. Clever <parchment@gmx.com>
2024-06-01 18:34:14 -0400

    Major routing update for book chapters

---
+++ app/static/js/bookRouting.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let currentURL = window.location.href;
+
+  if (currentURL.indexOf("/book#1") !== -1) {
+    document.getElementsByTagName('a')[1].click();
+  }
+
+  if (currentURL.indexOf("/book#2") !== -1) {
+    document.getElementsByTagName('a')[2].click();
+  }
+
+  if (currentURL.indexOf("/book#3") !== -1) {
+    document.getElementsByTagName('a')[3].click();
+  }
+
+  if (currentURL.indexOf("/book#4") !== -1) {
+    document.getElementsByTagName('a')[4].click();
+  }
+
+  if (currentURL.indexOf("/book#5") !== -1) {
+    document.getElementsByTagName('a')[5].click();
+  }
+
+  if (currentURL.indexOf("/book#6") !== -1) {
+    document.getElementsByTagName('a')[6].click();
+  }
+
+  if (currentURL.indexOf("/book#7") !== -1) {
+    document.getElementsByTagName('a')[7].click();
+  }
+
+  if (currentURL.indexOf("/book#8") !== -1) {
+    document.getElementsByTagName('a')[8].click();
+  }
+
+  if (currentURL.indexOf("/book#9") !== -1) {
+    document.getElementsByTagName('a')[9].click();
+  }
+
+  if (currentURL.indexOf("/book#10") !== -1) {
+    document.getElementsByTagName('a')[10].click();
+  }
+
+});
